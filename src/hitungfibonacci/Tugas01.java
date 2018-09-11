c20bfa8fdcf8c32d9ab1b0434c20cd81330cca93
ASUS <ASUS@LAPTOP-5VTS2EP5>
2018-09-11 17:34:35 +0700

    Initial Commit.

---
+++ src/hitungfibonacci/Tugas01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungfibonacci;
+
+public class Tugas01 {
+    private static void tampilJudul(String identitas)
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+        
+    }        
+    public static void main(String[] args) {
+        String identitas = "Salwa Nadira Adzani / X RPL 3 / 34";
+        
+        tampilJudul(identitas);
+    }
+}
